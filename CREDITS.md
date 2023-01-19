f37f3438911c677a082ec91052e021a4d3494a12
Winston Sung <winston21.sung@gmail.com>
2023-01-19 19:33:08 +0800

    Initial commit

---
+++ CREDITS.md
@@ -0,0 +1,14 @@
+TKUAIIC Posts i18n is a TKUAIIC project. We would like to recognize the
+following names for their contribution to the project.
+
+## Contributors
+<!-- BEGIN CONTRIBUTOR LIST -->
+* a2902793 (Johnny Yeng)
+* Chloe Fan
+* WinstonSung (Winston Sung)
+<!-- END CONTRIBUTOR LIST -->
+
+## Translators
+<!-- BEGIN TRANSLATOR LIST -->
+* Winston_Sung
+<!-- END TRANSLATOR LIST -->
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php # For broken web servers: ><pre>
+
+# If you are reading this in your web browser, your server is probably
+# not configured correctly to run PHP applications!
+#
+# See the README, INSTALL, and UPGRADE files for basic setup instructions
+# and pointers to the online documentation.
+#
+# https://www.mediawiki.org/wiki/Special:MyLanguage/MediaWiki
+#
+# -------------------------------------------------
+
+/**
+ * The main web entry point for web browser navigations, usually via an
+ * Action or SpecialPage subclass.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup entrypoint
+ */
+
+define( 'MW_ENTRY_POINT', 'index' );
+
+require __DIR__ . '/includes/webstart/WebStart.php';
+
+wfIndexMain();
+
+function wfIndexMain() {
+	$tkugers = new TKUGERS();
+	$tkugers->run();
+}
